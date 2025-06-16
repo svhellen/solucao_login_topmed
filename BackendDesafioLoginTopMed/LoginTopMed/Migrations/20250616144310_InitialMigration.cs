51a64778710cad9034d2c596c8c7e55a5d8df9ce
Cristiele <122491782+svhellen@users.noreply.github.com>
2025-06-16 13:28:55 -0300

    feat: add migrations

---
+++ BackendDesafioLoginTopMed/LoginTopMed/Migrations/20250616144310_InitialMigration.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace LoginTopMed.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Username = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    PasswordHash = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "CreatedAt", "Email", "IsActive", "Name", "PasswordHash", "Username" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2025, 6, 16, 14, 43, 7, 850, DateTimeKind.Utc).AddTicks(8226), "user1@topmed.com", true, "Maria", "$2a$11$P3xtyxCFM7.9oI9gz.T74OH6.hw1vnoq8PlmbkHrQBpWs66TfuGIa", "user_1" },
+                    { 2, new DateTime(2025, 6, 16, 14, 43, 8, 112, DateTimeKind.Utc).AddTicks(3267), "user2@topmed.com", true, "João", "$2a$11$FRztAeiIBSSXl8YZ/VpsBOozyeSRviWFG11F5E6FbbATU7ILey7ym", "user_2" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Email",
+                table: "Users",
+                column: "Email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Username",
+                table: "Users",
+                column: "Username",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
